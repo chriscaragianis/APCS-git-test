6af5371700074873c1d03ec7e63703bbad0ba54b
Chris Caragianis <chriscaragianis@gmail.com>
2015-11-10 07:18:29 -0500

    Fix GitTest class

--- GitTest.java
+++ GitTest.java
@@ -1,6 +1,6 @@
 class GitTest {
 
-  public static void main() {
+  public static void main(String[] args) {
     String name = "NAME GOES HERE";
     System.out.println("My name is " + name + " and I can use git");
   }
